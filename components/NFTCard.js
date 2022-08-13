1063b8bf0c8c3ef9a9fb2756c55e9ecb743064c7
JunaedAbed <junaedccpc17@gmail.com>
2022-08-14 03:32:30 +0600

    Sub Info component

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Image, Text, ImageStore } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
-const NFTCard = () => {
+import { COLORS, SHADOWS, SIZES, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+
+const NFTCard = ({ data }) => {
+  const navigatio = useNavigation();
+
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.small,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View style={{ width: "100%", height: 250 }}>
+        <Image
+          source={data.image}
+          resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }}
+        />
+
+        <CircleButton imgUrl={assets.heart} right={15} top={15} />
+      </View>
+
+      <SubInfo />
     </View>
   );
 };
