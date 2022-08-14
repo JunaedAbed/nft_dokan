260fbf27ce68db7bb2b4e66d0f3cf39fc399d371
JunaedAbed <junaedccpc17@gmail.com>
2022-08-15 00:10:09 +0600

    details screen added

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, TextInput } from "react-native";
 import { COLORS, SIZES, assets, FONTS } from "../constants";
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
   return (
     <View style={{ backgroundColor: COLORS.primary, padding: SIZES.font }}>
       <View
@@ -82,7 +82,7 @@ const HomeHeader = () => {
           <TextInput
             placeholder="Search NFTs"
             style={{ flex: 1 }}
-            onChangeText={() => {}}
+            onChangeText={onSearch}
           />
         </View>
       </View>
--- components/index.js
+++ components/index.js
@@ -1,6 +1,19 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
 import { CircleButton, RectButton } from "./Button";
 
-export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton };
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
